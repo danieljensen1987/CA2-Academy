9cb57c58e9b0779de0ac1b300e2c14b0f90eb745
danieljensen1987 <cph-dj74@cphbusiness.dk>
2014-10-02 16:14:11 +0200

    Modified: thows notFoundEx

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,8 +1,10 @@
 package server;
 
 import com.sun.net.httpserver.HttpServer;
+import exceptions.NotFoundException;
 import handlers.FileHandler;
 import handlers.PersonHandler;
+import handlers.RoleHandler;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
@@ -15,16 +17,17 @@ public class Server
 //    static String filesUri = "/pages";
     
     
-    public void run() throws IOException{
+    public void run() throws IOException, NotFoundException{
         HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
         server.createContext("/person" , new PersonHandler());
+        server.createContext("/role" , new RoleHandler());
         server.createContext("/", new FileHandler());
 //        server.createContext(filesUri, new FileHandler());
         server.start();
         System.out.println("Server is listening on port: " + port);
     }
     
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args) throws IOException, NotFoundException
     {
         if (args.length >= 3){
             port = Integer.parseInt(args[0]);
